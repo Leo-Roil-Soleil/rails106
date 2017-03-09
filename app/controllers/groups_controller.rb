c830eb40598661124fe640cd8496aac4f10a747e
Leo-Roil-Soleil <leokexin@gmail.com>
2017-03-09 09:13:57 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,47 +1,49 @@
 class GroupsController < ApplicationController
-  def index
-    @groups = Group.all
-  end
+    def index
+        @groups = Group.all
+    end
 
-  def show
-   @group = Group.find(params[:id])
- end
-
- def edit
-   @group = Group.find(params[:id])
- end
-
-  def new
-    @group = Group.new
-  end
-
-   def create
-   @group = Group.new(group_params)
-   if @group.save
-     redirect_to groups_path
-   else
-     render :new
+    def show
+        @group = Group.find(params[:id])
    end
- end
 
-   def update
-    @group = Group.find(params[:id])
+    def edit
+        @group = Group.find(params[:id])
+    end
 
-    @group.update(group_params)
+    def new
+        @group = Group.new
+    end
 
-    redirect_to groups_path, notice: "Update Success"
+    def create
+        @group = Group.new(group_params)
+        if @group.save
+            redirect_to groups_path
+        else
+            render :new
+        end
   end
+
+    def update
+        @group = Group.find(params[:id])
+
+        if @group.update(group_params)
+            redirect_to groups_path, notice: 'Update Success'
+        else
+            render :edit
+     end
+   end
 
     def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:alert] = 'Group deleted'
+        redirect_to groups_path
   end
 
- private
+    private
 
- def group_params
-   params.require(:group).permit(:title, :description)
- end
+    def group_params
+        params.require(:group).permit(:title, :description)
+    end
 end
